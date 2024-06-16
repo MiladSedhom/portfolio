1c60ecbb5bed4065fb21e95c1f8f42b5da309891
MiladSedhom <MiladSedhom@gmail.com>
2024-06-16 19:00:22 +0300

    initial commit

---
+++ uno.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'unocss';
+import { presetUno, presetAttributify, presetWebFonts, presetIcons } from 'unocss';
+
+export default defineConfig({
+	presets: [
+		presetUno(),
+		presetAttributify(),
+		presetWebFonts({
+			provider: 'google',
+			fonts: {
+				serif: 'Bricolage Grotesque',
+				sans: 'Poppins'
+			}
+		}),
+		presetIcons({
+			extraProperties: {
+				display: 'inline-block',
+				'vertical-align': 'middle'
+			}
+		})
+	],
+	theme: {
+		colors: {
+			primary: 'var(--primary)',
+			tcolor: 'var(--text)',
+			'tcolor-alt': 'var(--text-alt)',
+			bgcolor: 'var(--bg)'
+		}
+	}
+});
