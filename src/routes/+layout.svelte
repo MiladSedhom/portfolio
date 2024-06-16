4605191d479b90ad196b98f997b17e0728e4cb61
MiladSedhom <MiladSedhom@gmail.com>
2024-06-16 23:49:36 +0300

    sketching homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import 'virtual:uno.css';
+	import '@unocss/reset/tailwind.css';
+</script>
+
+<div class="min-h-screen min-w-screen bg-bgcolor font-sans color-tcolor">
+	<header class=" h-16 p-4"></header>
+	<slot />
+</div>
+
+<style>
+	:global(:root) {
+		--h: 29;
+		--primary: hsl(var(--h), 65%, 45%);
+		--bg: hsl(var(--h), 8%, 95%);
+		--text: hsl(var(--h), 8%, 15%);
+		--text-alt: hsl(var(--h), 8%, 95%);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main p-4 max-w-65ch m-auto>
+	<h1 text="tcolor 12" fw-bold font-display>Milad Sedhom</h1>
+	<p>Hi there, I am a web deverloper, passionate about creating pretty UIs</p>
+
+	<div p-10></div>
+
+	<section flex="~ col gap-8">
+		<div
+			class="flex min-h-50 p-4 bg-green-600"
+			outline="2 solid offset--8"
+			hover="rotate--3"
+			hover-shadow="green-900 xl opacity-20"
+			transition="property-transform property-shadow 150 ease-in"
+			text-3
+		>
+			<a href="https://github.com/MiladSedhom/PennyPal" target="_blank">
+				<h1 fw-bold text-8 font-display>PennyPal</h1>
+				<p>a modern and intutive expenses tracker</p>
+			</a>
+		</div>
+
+		<div
+			style="--color: #219C84"
+			class="flex min-h-50 p-4 bg-[var(--color)]"
+			outline="2 solid offset--8"
+			hover="rotate--3"
+			hover-shadow="#219C84 xl opacity-20"
+			transition="property-transform property-shadow 150 ease-in"
+			text="3 tcolor-alt"
+		>
+			<a href="https://github.com/MiladSedhom/grad-project" target="_blank">
+				<h1 fw-bold text-8 font-display>Science Talk</h1>
+				<p>Social media application for students and students only.</p>
+				<p>
+					Was made by a team of 2 as a graduation project for Department of Physcics and Computer
+					Science, Facultiy of Science, Ain Shams unveristy
+				</p>
+			</a>
+		</div>
+	</section>
+</main>
