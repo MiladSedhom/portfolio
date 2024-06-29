2219af74da0ffda454275202cc33c0c1b9c94329
MiladSedhom <MiladSedhom@gmail.com>
2024-06-29 18:37:16 +0300

    refactor: mouseSticky in Project componant

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -1,43 +1,26 @@
 <script lang="ts">
-	import { mapRange } from '$lib/utils';
+	import { mouseSticky } from '$lib/actions/mouseSticky.svelte';
 
-	const { name, description, link, class: classes, ...rest }: Props = $props();
-
-	const mouse = $state<{ x: number; y: number }>({ x: 0, y: 0 });
-
-	let div: HTMLDivElement;
-	let divRect: DOMRect;
-
-	$effect(() => {
-		divRect = div.getBoundingClientRect();
-	});
-
-	const mouseMoveHandler = (e: MouseEvent) => {
-		const mouseX = e.clientX;
-		const mouseY = e.clientY;
-
-		mouse.x = mapRange((mouseX - divRect.x) / divRect.width, 0, 1, -1, 1);
-		mouse.y = mapRange((mouseY - divRect.y) / divRect.height, 0, 1, -1, 1);
-	};
+	const {
+		name,
+		description,
+		link,
+		class: classes = undefined,
+		body = undefined,
+		...rest
+	}: Props = $props();
 
 	type Props = {
 		name: string;
 		description: string;
 		link: string;
-		class: string;
+		class?: string;
+		body?: string;
 	};
 </script>
 
 <div
-	onmousemove={mouseMoveHandler}
-	onmouseleave={() => {
-		mouse.x = 0;
-		mouse.y = 0;
-	}}
-	style={`transform: translate(${8 * mouse.x}px, ${4 * mouse.y}px) rotate(${-1 * mouse.x}deg) ;
-                ${mouse.x + mouse.y && 'transition-duration: 0s;'} `}
-	role="banner"
-	bind:this={div}
+	use:mouseSticky
 	class="h-50 min-h-50 outline-2 outline-solid text-3 {classes}"
 	hover-shadow="xl opacity-20"
 	transition="property-transform 300 ease-in"
@@ -46,5 +29,8 @@
 	<a href={link} target="_blank" class="block min-h-full p-4">
 		<h1 fw-bold text-8 font-display>{name}</h1>
 		<p>{description}</p>
+		{#if body}
+			<p>{body}</p>
+		{/if}
 	</a>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,5 @@
 <script lang="ts">
-	const mouse = $state<{ x: number; y: number }>({ x: 0, y: 0 });
-	let d = $state();
-	let div: HTMLDivElement;
-
-	$effect(() => {
-		d = div.getBoundingClientRect();
-	});
-
-	function mapRange(value: number, oldMin: number, oldMax: number, newMin: number, newMax: number) {
-		return newMin + ((newMax - newMin) * (value - oldMin)) / (oldMax - oldMin);
-	}
-
-	const mouseMoveHandler = (e: MouseEvent) => {
-		const mouseX = e.clientX;
-		const mouseY = e.clientY;
-
-		mouse.x = mapRange((mouseX - d.x) / d.width, 0, 1, -1, 1);
-		mouse.y = mapRange((mouseY - d.y) / d.height, 0, 1, -1, 1);
-	};
+	import Project from '$lib/components/Project.svelte';
 </script>
 
 <main p-4 max-w-65ch m-auto>
@@ -27,46 +9,18 @@
 	<div p-10></div>
 
 	<section flex="~ col gap-8">
-		<div
-			onmousemove={mouseMoveHandler}
-			onmouseleave={() => {
-				mouse.x = 0;
-				mouse.y = 0;
-			}}
-			style={` translate: ${4 * mouse.x}px ${4 * mouse.y}px;
-					 rotate: ${-1 * mouse.x}deg;
-				`}
-			role="banner"
-			bind:this={div}
-			class="flex min-h-50 p-4 bg-green-600"
-			outline="2 solid "
-			hover-shadow="green-900 xl opacity-20"
-			transition="property-transform property-shadow 150 ease-in"
-			text-3
-		>
-			<a href="https://github.com/MiladSedhom/PennyPal" target="_blank">
-				<h1 fw-bold text-8 font-display>PennyPal</h1>
-				<p>a modern and intutive expenses tracker</p>
-			</a>
-		</div>
-
-		<div
-			style="--color: #219C84"
-			class="flex min-h-50 p-4 bg-[var(--color)]"
-			outline="2 solid "
-			hover="rotate--2"
-			hover-shadow="#219C84 xl opacity-20"
-			transition="property-transform property-shadow 150 ease-in"
-			text="3 tcolor-alt"
-		>
-			<a href="https://github.com/MiladSedhom/grad-project" target="_blank">
-				<h1 fw-bold text-8 font-display>Science Talk</h1>
-				<p>Social media application for students and students only.</p>
-				<p>
-					Was made by a team of 2 as a graduation project for Department of Physcics and Computer
-					Science, Facultiy of Science, Ain Shams unveristy
-				</p>
-			</a>
-		</div>
+		<Project
+			class="bg-green-600"
+			name="PennyPal"
+			description="a modern and intutive expenses tracker"
+			link="https://github.com/MiladSedhom/PennyPal"
+		/>
+		<Project
+			class="bg-#219C84"
+			name="Science Talk"
+			description="Social media application for students and students only."
+			link="https://github.com/MiladSedhom/grad-project"
+			body="Was made by a team of 2 as a graduation project for Department of Physcics and Computer Science, Facultiy of Science, Ain Shams unveristy"
+		/>
 	</section>
 </main>
