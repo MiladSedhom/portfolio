92837d1e56aaa7122fd22ca96afb2694deacf6db
MiladSedhom <MiladSedhom@gmail.com>
2024-07-17 21:27:10 +0300

    Updates

--- src/lib/utils/index.ts
+++
@@ -1,9 +0,0 @@
-export const mapRange = (
-	value: number,
-	oldMin: number,
-	oldMax: number,
-	newMin: number,
-	newMax: number
-) => {
-	return newMin + ((newMax - newMin) * (value - oldMin)) / (oldMax - oldMin)
-}
