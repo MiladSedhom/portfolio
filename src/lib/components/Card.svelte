cd2157ee0327be671996a2bcd7706f0a5344b9ca
MiladSedhom <MiladSedhom@gmail.com>
2024-07-17 18:35:38 +0300

    refactor Project 		Component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,38 +1,19 @@
 <script lang="ts">
 	import { getRelativeMouse } from '$lib/getRelativeMouse.svelte'
-	import { onMount } from 'svelte'
+	import { type Snippet } from 'svelte'
 
 	type Props = {
-		name: string
-		description: string
-		link: string
 		class?: string
-		body?: string
+		children?: Snippet
 	}
 
-	const {
-		name,
-		description,
-		link,
-		class: classes = undefined,
-		body = undefined,
-		...rest
-	}: Props = $props()
+	const { class: classes = undefined, children, ...rest }: Props = $props()
 
 	let container = $state<HTMLElement>()
 	let div = $state<HTMLElement>()
 	let animationDone = $state(false)
-	let isIn = $state<boolean>()
 	let mouse = getRelativeMouse(() => container, { min: -1, max: 1 })
 
-	onMount(() => {
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((e) => {
-				if (e.isIntersecting) isIn = e.isIntersecting
-			})
-		})
-		container && observer.observe(container)
-	})
 	$effect(() => {
 		if (!div) return
 		if (mouse.isInside) div.style.transition = 'transform 0ms ease-out'
@@ -43,24 +24,18 @@
 <div bind:this={container} class="fly-in bg-shadows border-rd-xl">
 	<div
 		bind:this={div}
+		class="min-h-25 text-3 border-rd-xl {classes}"
+		class:fly-in-delayed={!animationDone}
+		style:opacity={animationDone ? '1' : '0'}
 		style="transform: perspective(3000px) rotateX({-20 * mouse.y}deg) rotateY({10 *
 			mouse.x}deg) {mouse.isInside ? 'scale(.99)' : ''};"
-		style:opacity={animationDone ? '1' : '0'}
-		class:fly-in-delayed={!animationDone}
-		class="min-h-25 text-3 border-rd-xl {classes}"
 		hover-shadow="xl opacity-20"
 		onanimationend={() => {
 			animationDone = true
 		}}
 		{...rest}
 	>
-		<a href={link} target="_blank" class="block min-h-full p-4">
-			<h1 fw-bold text-5 font-display>{name}</h1>
-			<p text-14px fw-semibolds>{description}</p>
-			{#if body}
-				<p text-3>{body}</p>
-			{/if}
-		</a>
+		{@render children?.()}
 	</div>
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
-	import Project from '$lib/components/Project.svelte'
+	import Card from '$lib/components/Card.svelte'
 </script>
 
 <main p-4 max-w-65ch m-auto>
-	<h1
-		class="slide-prime text-tcolor text-6 fw-bold font-display inline-block m-b-6
+	<div flex justify-between items-start m-b-6>
+		<h1
+			class="slide-prime text-tcolor text-6 fw-bold font-display inline-block
 		pos-relative"
-		underline="~ wavy 2 orange-600 offset-4"
-	>
-		Milad Sedhom
-	</h1>
+			underline="~ wavy 2 primary offset-4"
+		>
+			Milad Sedhom
+		</h1>
+
+		<a
+			href="http://github.com/MiladSedhom"
+			target="_blank"
+			class="border-rd-50% line-height-1 p-2"
+			hover-outline="dotted 3 primary offset--2"
+		>
+			<div class="i-logos-github-icon?mask bg-tcolor text-5"></div>
+		</a>
+	</div>
 
 	<p text-tcolor-1>
 		Hi there, I am a fullstack web developer, passionate about creating pretty UIs and unique user
@@ -20,19 +31,44 @@
 
 	<h2 class="text-4 fw-bold m-b-4 font-display">Projects</h2>
 	<section flex="~ col gap-6">
-		<Project
-			class="border-dashed border-3 border-green-600 bg-bgcolor"
-			name="PennyPal"
-			description="a modern and intuitive expenses tracker"
-			link="https://github.com/MiladSedhom/PennyPal"
-		/>
-		<Project
-			class="border-dashed border-3 border-#219C84 bg-bgcolor"
-			name="Science Talk"
-			description="Social media application for students and students only."
-			link="https://github.com/MiladSedhom/grad-project"
-			body="Was made by a team of 2 as a graduation project for Department of Physcics and Computer Science, Facultiy of Science, Ain Shams unveristy"
-		/>
+		<Card class="border-dashed border-3 border-green-600 bg-bgcolor">
+			<a
+				href="https://github.com/MiladSedhom/PennyPal"
+				target="_blank"
+				class="block min-h-full p-4"
+			>
+				<div flex justify-between>
+					<h1 fw-bold text-5 font-display>PennyPal</h1>
+					<div p-2>
+						<div class="i-logos-svelte-icon text-5"></div>
+						<div class="i-logos-prisma text-5"></div>
+						<div class="i-logos-supabase-icon?mask bg-#288e5c text-5"></div>
+					</div>
+				</div>
+				<p text-14px>a modern and intuitive expenses tracker</p>
+			</a>
+		</Card>
+		<Card class="border-dashed border-3 border-#219C84 bg-bgcolor">
+			<a
+				href="https://github.com/MiladSedhom/grad-project"
+				target="_blank"
+				class="block min-h-full p-4"
+			>
+				<div flex justify-between>
+					<h1 fw-bold text-5 font-display>Science Talk</h1>
+					<div p-2>
+						<div class="i-logos-svelte-icon text-5"></div>
+						<div class="i-logos-prisma text-5"></div>
+						<div class="i-logos-supabase-icon text-5"></div>
+					</div>
+				</div>
+				<p text-14px>Social media application for students and students only.</p>
+				<p text-3>
+					Was made by a team of 2 as a graduation project for Department of Physcics and Computer
+					Science, Facultiy of Science, Ain Shams unveristy
+				</p>
+			</a>
+		</Card>
 	</section>
 </main>
 
