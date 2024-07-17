c84ad5cebffbe95b15f2a0b78c9a1f186a8056b5
MiladSedhom <MiladSedhom@gmail.com>
2024-07-17 20:42:32 +0300

    typos and supabase colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,21 +5,31 @@
 <main p-4 max-w-65ch m-auto>
 	<div flex justify-between items-start m-b-6>
 		<h1
-			class="slide-prime text-tcolor text-6 fw-bold font-display inline-block
-		pos-relative"
+			class="slide-prime-left text-tcolor text-6 fw-bold font-display inline-block
+					pos-relative"
 			underline="~ wavy 2 primary offset-4"
 		>
 			Milad Sedhom
 		</h1>
 
-		<a
-			href="http://github.com/MiladSedhom"
-			target="_blank"
-			class="border-rd-50% line-height-1 p-2"
-			hover-outline="dotted 3 primary offset--2"
-		>
-			<div class="i-logos-github-icon?mask bg-tcolor text-5"></div>
-		</a>
+		<div class="slide-prime-right">
+			<a
+				href="http://x.com/winter1v9"
+				target="_blank"
+				class="inline-block border-rd-50% p-2 line-height-1"
+				hover-outline="dotted 3 primary offset--2"
+			>
+				<div class="i-logos-twitter?mask bg-tcolor text-5"></div>
+			</a>
+			<a
+				href="http://github.com/MiladSedhom"
+				target="_blank"
+				class="inline-block border-rd-50% p-2 line-height-1"
+				hover-outline="dotted 3 primary offset--2"
+			>
+				<div class="i-logos-github-icon?mask bg-tcolor text-5"></div>
+			</a>
+		</div>
 	</div>
 
 	<p text-tcolor-1>
@@ -59,13 +69,13 @@
 					<div p-2>
 						<div class="i-logos-svelte-icon text-5"></div>
 						<div class="i-logos-prisma text-5"></div>
-						<div class="i-logos-supabase-icon text-5"></div>
+						<div class="i-logos-supabase-icon?mask bg-#288e5c text-5"></div>
 					</div>
 				</div>
 				<p text-14px>Social media application for students and students only.</p>
 				<p text-3>
-					Was made by a team of 2 as a graduation project for Department of Physcics and Computer
-					Science, Facultiy of Science, Ain Shams unveristy
+					Was made by a team of 2 as a graduation project for Department of Physics and Computer
+					Science, Faculty of Science, Ain Shams university
 				</p>
 			</a>
 		</Card>
@@ -73,18 +83,29 @@
 </main>
 
 <style>
-	.slide-prime {
-		animation: slide-prime 1000ms cubic-bezier(0.23, 1, 0.32, 1);
+	.slide-prime-left {
+		animation: slide-prime-left 1000ms cubic-bezier(0.23, 1, 0.32, 1);
 	}
-	@keyframes slide-prime {
+	@keyframes slide-prime-left {
 		0% {
 			clip-path: inset(0 100% 0 100%);
 			transform: translateX(-100%);
 		}
-		/* 90% {
-			clip-path: inset(round 2rem);
-			transform: translateX(15px);
-		} */
+
+		100% {
+			clip-path: inset(0);
+			transform: translateX(0);
+		}
+	}
+	.slide-prime-right {
+		animation: slide-prime-right 1000ms cubic-bezier(0.23, 1, 0.32, 1);
+	}
+	@keyframes slide-prime-right {
+		0% {
+			clip-path: inset(0 100% 0 100%);
+			transform: translateX(100%);
+		}
+
 		100% {
 			clip-path: inset(0);
 			transform: translateX(0);
